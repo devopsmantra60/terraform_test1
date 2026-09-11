f1abc0eb65aac8d604f8f854c951d4e31c7edba4
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-11 21:21:53 +0530

    added main, provider and gitignore files

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "adcaa770-60c9-482f-a61d-454735649e99"
+}
