63c9bbf247ce9b2c04f1be9723793a578fc8aced
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-11 22:59:46 +0530

    added storage

--- main.tf
+++ main.tf
@@ -3,3 +3,14 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "storage0101maccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
