8230821e0940746a69dceee000d694a39597ba91
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-12 12:38:11 +0530

    changed location and storage name

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
   name     = "Test-RG"
-  location = "West Europe"
+  location = "Japan East"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storage0101maccountname"
+  name                     = "storage010101maccountname"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
